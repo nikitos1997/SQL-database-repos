7bd24a40423cb0f8544ea44b768b522f1973dbbb
Skripnikov, Nikita <293779@student.lincolncollege.ac.uk>
2019-01-09 09:28:52 +0000

    Create database.sql
    Base file

---
+++ database.sql
@@ -0,0 +1,112 @@
+DROP DATABASE IF EXISTS BursaryDatabase;
+CREATE DATABASE BursaryDatabase; /*Creating the database*/
+USE BursaryDatabase;
+
+
+DROP TABLE IF EXISTS Students CASCADE;
+
+/*Creating students table*/
+CREATE TABLE Students (
+  StudentID INTEGER NOT NULL AUTO_INCREMENT, 
+  StudentFirstName VARCHAR(15) NOT NULL, 
+  StudentLastName VARCHAR(25) NOT NULL, 
+  DOB DATE NOT NULL,
+  GENDER BOOLEAN NOT NULL, 
+  AvailableBalance DECIMAL(5,2) NOT NULL, 
+  PRIMARY KEY (StudentID)
+);
+
+DROP TABLE IF EXISTS Tutors CASCADE;
+
+/*Creating tutors table */
+CREATE TABLE Tutors (
+  TutorID INTEGER NOT NULL AUTO_INCREMENT, 
+  TutorFirstName VARCHAR(15) NOT NULL, 
+  TutorLastName VARCHAR(25) NOT NULL, 
+  PRIMARY KEY (TutorID)
+);
+
+DROP TABLE IF EXISTS Courses CASCADE;
+
+/*Creating Courses table */
+CREATE TABLE Courses (
+  CourseID INTEGER NOT NULL AUTO_INCREMENT, 
+  CourseTitle VARCHAR(35) NOT NULL, 
+  Subject VARCHAR(25) NOT NULL, 
+  FullTime/PartTime BOOLEAN NOT NULL, 
+  Year DATE NOT NULL, 
+  PRIMARY KEY (CourseID) 
+); 
+
+DROP TABLE IF EXISTS Departments CASCADE;
+
+/*Creating departments table */
+CREATE TABLE Departments (
+  DepartmentID INTEGER NOT NULL AUTO_INCREMENT, 
+  DepartmentName VARCHAR(35) NOT NULL, 
+  CampusName VARCHAR(30) NOT NULL, 
+  PRIMARY KEY (DepartmentID),
+);
+
+DROP TABLE IF EXISTS BursaryRequests CASCADE;
+
+/*Creating bursary requests table */
+CREATE TABLE BursaryRequests (
+  RequestID INTEGER NOT NULL AUTO_INCREMENT, 
+  CourseID INTEGER NOT NULL, 
+  TutorID INTEGER NOT NULL, 
+  Justification VARCHAR(200) NOT NULL, 
+  TutorComments VARCHAR(200), 
+  AdminComments VARCHAR(200), 
+  StaffApproved BOOLEAN DEFAULT NULL, 
+  AdminApproved BOOLEAN DEFAULT NULL,
+  Ordered BOOLEAN DEFAULT NULL,
+  Delivered BOOLEAN DEFAULT NULL, 
+  RequestDate DATE NOT NULL, 
+  Draft BOOLEAN DEFAULT NULL,  
+  PRIMARY KEY (RequestID),
+  FOREIGN KEY (CourseID) REFERENCES Courses(CourseID),
+  FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID)
+);
+
+DROP TABLE IF EXISTS BursaryRequestItems CASCADE;
+
+/*Creating bursary request items table */
+CREATE TABLE BursaryRequestItems (
+  ItemID INTEGER NOT NULL AUTO_INCREMENT, 
+  Category VARCHAR(30) NOT NULL, 
+  ItemDesc VARCHAR(100) NOT NULL,
+  ItemURL VARCHAR(200) NOT NULL, 
+  Price DECIMAL(5,2) NOT NULL,
+  Postage DECIMAL(5,2) DEFAULT NULL,
+  AdditionalCharges DECIMAL(5,2) DEFAULT NULL,   
+  PRIMARY KEY (ItemID)
+);
+
+DROP TABLE IF EXISTS ItemsAndRequests CASCADE;
+
+/*Creating items with requests table table*/
+CREATE TABLE ItemsAndRequests (
+  ItemID INTEGER NOT NULL, 
+  RequestID INTEGER NOT NULL, 
+  StudentID INTEGER NOT NULL, 
+  PRIMARY KEY(ItemID, RequestID, StudentID),
+  FOREIGN KEY(ItemID) REFERENCES BursaryRequestItems(ItemID),
+  FOREIGN KEY(RequestID) REFERENCES BursaryRequests(RequestID),
+  FOREIGN KEY(StudentID) REFERENCES Students(StudentID)
+);
+
+DROP TABLE IF EXISTS DepartmentsWithStudentsCoursesAndTutors CASCADE;
+
+/*Creating departs with students, courses and tutors table */
+CREATE TABLE DepartmentsWithStudentsCoursesAndTutors (
+  DepartmentID INTEGER NOT NULL, 
+  TutorID INTEGER NOT NULL, 
+  StudentID INTEGER NOT NULL, 
+  CourseID INTEGER NOT NULL,
+  PRIMARY KEY (DepartmentID, TutorID, StudentID, CourseID),
+  FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
+  FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID),
+  FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+  FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
