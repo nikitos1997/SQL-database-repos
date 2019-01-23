fc28b88095768e0fa6a06e0c1f159273c6f12934
Skripnikov, Nikita <293779@student.lincolncollege.ac.uk>
2019-01-23 09:59:38 +0000

    Commiting CSV changes
    Updating the database according to given CSV.

--- database.sql
+++ database.sql
@@ -7,45 +7,50 @@ DROP TABLE IF EXISTS Students CASCADE;
 
 /*Creating students table*/
 CREATE TABLE Students (
-  StudentID INTEGER NOT NULL AUTO_INCREMENT, 
+  StudentID INTEGER NOT NULL UNIQUE, 
   StudentFirstName VARCHAR(15) NOT NULL, 
   StudentLastName VARCHAR(25) NOT NULL, 
   DOB DATE NOT NULL,
   GENDER BOOLEAN NOT NULL, 
   AvailableBalance DECIMAL(5,2) NOT NULL, 
-  PRIMARY KEY (StudentID)
+  PRIMARY KEY (StudentID),
+  UNIQUE KEY 'StudentID' ('StudentID')
 );
 
 DROP TABLE IF EXISTS Tutors CASCADE;
 
 /*Creating tutors table */
 CREATE TABLE Tutors (
-  TutorID INTEGER NOT NULL AUTO_INCREMENT, 
+  TutorID INTEGER NOT NULL UNIQUE, 
   TutorFirstName VARCHAR(15) NOT NULL, 
   TutorLastName VARCHAR(25) NOT NULL, 
-  PRIMARY KEY (TutorID)
+  PRIMARY KEY (TutorID),
+  UNIQUE KEY 'TutorID' ('TutorID')
 );
 
 DROP TABLE IF EXISTS Courses CASCADE;
 
 /*Creating Courses table */
 CREATE TABLE Courses (
-  CourseID INTEGER NOT NULL AUTO_INCREMENT, 
+  CourseID VARCHAR(255) NOT NULL UNIQUE, 
   CourseTitle VARCHAR(35) NOT NULL, 
   Subject VARCHAR(25) NOT NULL, 
   FullTime/PartTime BOOLEAN NOT NULL, 
-  Year DATE NOT NULL, 
-  PRIMARY KEY (CourseID) 
+  StartDate DATE NOT NULL,
+  EndDate DATE NOT NULL,
+  PRIMARY KEY (CourseID),
+  UNIQUE KEY 'CourseID' ('CourseID') 
 ); 
 
 DROP TABLE IF EXISTS Departments CASCADE;
 
 /*Creating departments table */
 CREATE TABLE Departments (
-  DepartmentID INTEGER NOT NULL AUTO_INCREMENT, 
+  DepartmentID VARCHAR(255) NOT NULL UNIQUE, 
   DepartmentName VARCHAR(35) NOT NULL, 
   CampusName VARCHAR(30) NOT NULL, 
   PRIMARY KEY (DepartmentID),
+  UNIQUE KEY 'DepartmentID' ('DepartmentID')
 );
 
 DROP TABLE IF EXISTS BursaryRequests CASCADE;
@@ -53,7 +58,7 @@ DROP TABLE IF EXISTS BursaryRequests CASCADE;
 /*Creating bursary requests table */
 CREATE TABLE BursaryRequests (
   RequestID INTEGER NOT NULL AUTO_INCREMENT, 
-  CourseID INTEGER NOT NULL, 
+  CourseID VARCHAR(255) NOT NULL, 
   TutorID INTEGER NOT NULL, 
   Justification VARCHAR(200) NOT NULL, 
   TutorComments VARCHAR(200), 
@@ -100,10 +105,11 @@ DROP TABLE IF EXISTS DepartmentsWithStudentsCoursesAndTutors CASCADE;
 
 /*Creating departs with students, courses and tutors table */
 CREATE TABLE DepartmentsWithStudentsCoursesAndTutors (
-  DepartmentID INTEGER NOT NULL, 
+  DepartmentID VARCHAR(255) NOT NULL, 
   TutorID INTEGER NOT NULL, 
   StudentID INTEGER NOT NULL, 
-  CourseID INTEGER NOT NULL,
+  CourseID VARCHAR(255) NOT NULL,
+  StudentStatus VARCHAR(20) NOT NULL,
   PRIMARY KEY (DepartmentID, TutorID, StudentID, CourseID),
   FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
   FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID),
