4418d8a92d7580517f335a7214f9b861c867e8b5
Skripnikov, Nikita <293779@student.lincolncollege.ac.uk>
2019-01-23 10:37:43 +0000

    Changes to database after review
    Each item now has an ability to be approved and or rejected. Tutors table is changed to be named Staff. Staff approved and admin approved attributes changed to varchar instead of boolean.

--- database.sql
+++ database.sql
@@ -20,12 +20,12 @@ CREATE TABLE Students (
 DROP TABLE IF EXISTS Tutors CASCADE;
 
 /*Creating tutors table */
-CREATE TABLE Tutors (
-  TutorID INTEGER NOT NULL UNIQUE, 
-  TutorFirstName VARCHAR(15) NOT NULL, 
-  TutorLastName VARCHAR(25) NOT NULL, 
+CREATE TABLE Staff (
+  StaffID INTEGER NOT NULL UNIQUE, 
+  StaffFirstName VARCHAR(15) NOT NULL, 
+  StaffLastName VARCHAR(25) NOT NULL, 
   PRIMARY KEY (TutorID),
-  UNIQUE KEY 'TutorID' ('TutorID')
+  UNIQUE KEY 'StaffID' ('StaffID')
 );
 
 DROP TABLE IF EXISTS Courses CASCADE;
@@ -59,19 +59,19 @@ DROP TABLE IF EXISTS BursaryRequests CASCADE;
 CREATE TABLE BursaryRequests (
   RequestID INTEGER NOT NULL AUTO_INCREMENT, 
   CourseID VARCHAR(255) NOT NULL, 
-  TutorID INTEGER NOT NULL, 
+  StaffID INTEGER NOT NULL, 
   Justification VARCHAR(200) NOT NULL, 
   TutorComments VARCHAR(200), 
   AdminComments VARCHAR(200), 
-  StaffApproved BOOLEAN DEFAULT NULL, 
-  AdminApproved BOOLEAN DEFAULT NULL,
+  StaffApproved VARCHAR(3) DEFAULT NULL, 
+  AdminApproved VARCHAR(3) DEFAULT NULL,
   Ordered BOOLEAN DEFAULT NULL,
   Delivered BOOLEAN DEFAULT NULL, 
   RequestDate DATE NOT NULL, 
   Draft BOOLEAN DEFAULT NULL,  
   PRIMARY KEY (RequestID),
   FOREIGN KEY (CourseID) REFERENCES Courses(CourseID),
-  FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID)
+  FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
 );
 
 DROP TABLE IF EXISTS BursaryRequestItems CASCADE;
@@ -95,6 +95,8 @@ CREATE TABLE ItemsAndRequests (
   ItemID INTEGER NOT NULL, 
   RequestID INTEGER NOT NULL, 
   StudentID INTEGER NOT NULL, 
+  StaffItemApproved VARCHAR(3) DEFAULT NULL,
+  AdminItemApproved VARCHAR(3) DEFAULT NULL,
   PRIMARY KEY(ItemID, RequestID, StudentID),
   FOREIGN KEY(ItemID) REFERENCES BursaryRequestItems(ItemID),
   FOREIGN KEY(RequestID) REFERENCES BursaryRequests(RequestID),
@@ -106,13 +108,13 @@ DROP TABLE IF EXISTS DepartmentsWithStudentsCoursesAndTutors CASCADE;
 /*Creating departs with students, courses and tutors table */
 CREATE TABLE DepartmentsWithStudentsCoursesAndTutors (
   DepartmentID VARCHAR(255) NOT NULL, 
-  TutorID INTEGER NOT NULL, 
+  StaffID INTEGER NOT NULL, 
   StudentID INTEGER NOT NULL, 
   CourseID VARCHAR(255) NOT NULL,
   StudentStatus VARCHAR(20) NOT NULL,
   PRIMARY KEY (DepartmentID, TutorID, StudentID, CourseID),
   FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
-  FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID),
+  FOREIGN KEY (StaffID) REFERENCES Staff(StaffID),
   FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
   FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
 );
