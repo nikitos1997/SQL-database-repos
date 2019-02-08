a8eb8f9390e9feb702bcba947a98e4ae2c3bb139
Skripnikov, Nikita <293779@student.lincolncollege.ac.uk>
2019-02-08 20:15:43 +0000

    Update nickQueries.sql
    New queries added and some fixed

--- nickQueries.sql
+++ nickQueries.sql
@@ -13,7 +13,7 @@ SELECT CONCAT(userFirstName, " ", userLastName) as 'Name' from users
 where userID = 293779;
 /*FOR PHP*/
 /*SELECT CONCAT(userFirstName, " ", userLastName) as 'Name' from users
-where userID = $UserID;*/
+where userID = $userid;*/
 
 /*Select course title of a specific student TEST*/
 SELECT courseTitle as 'Course' from course inner join studentToCourse
@@ -22,7 +22,7 @@ on users.userID = 293779 and studentToCourse.stcStudentID = 293779;
 /*FOR PHP*/
 /*SELECT courseTitle as 'Course' from course inner join studentToCourse
 on course.courseID = studentToCourse.stcCourseID inner join users 
-on users.userID = $UserID and studentToCourse.stcStudentID = $UserID;*/
+on users.userID = $UserID and studentToCourse.stcStudentID = $userid;*/
 
 /*COUNT all bursary requests that are not approved (Submitted) NOT DRAFT as well TEST */
 SELECT COUNT(*) as 'Total' from bursaryRequests inner join itemsAndRequests
@@ -31,7 +31,7 @@ and bursaryRequests.bRequestsStaffApproved is NULL and bursaryRequests.bRequests
 and bRequestsDraft is NULL;
 /*FOR PHP*/
 /*SELECT COUNT(*) as 'Total' from bursaryRequests inner join itemsAndRequests
-where itemsAndRequests.RequestID = bursaryRequests.bRequestsID and itemsAndRequests.StudentID = $UserID
+where itemsAndRequests.RequestID = bursaryRequests.bRequestsID and itemsAndRequests.StudentID = $userid
 and bursaryRequests.bRequestsStaffApproved is NULL and bursaryRequests.bRequestsAdminApproved is NULL 
 and bRequestsDraft is NULL;*/
 
@@ -55,8 +55,8 @@ bRequestsRequestDate as 'Request date' from bursaryRequests
 inner join course on bursaryRequests.bRequestsCourseID = course.courseID
 inner join users on users.userID = bursaryRequests.bRequestsStaffID
 inner join itemsAndRequests on itemsAndRequests.RequestID = $RequestID 
-and bursaryRequests.bRequestsID = $RequestID
-and itemsAndRequests.StudentID = $UserID*/
+and bursaryRequests.bRequestsID = $requestid
+and itemsAndRequests.StudentID = $userid*/
 
 /*OUTPUT all items linked to a specific bursary request of a specific student TEST */
 SELECT brItemCategory as 'Category', brItemDesc as 'Item description',
@@ -70,7 +70,26 @@ and itemsAndRequests.StudentID = 29000;
 brItemURL as 'URL', brItemPrice as 'Price', brItemPostage as 'Postage',
 brItemAdditionalCharges as 'Additional charges' from bursaryRequestItems
 inner join itemsAndRequests on itemsAndRequests.ItemID = bursaryRequestItems.brItemID 
-and itemsAndRequests.RequestID = $RequestID
-and itemsAndRequests.StudentID = $UserID;*/
+and itemsAndRequests.RequestID = $requestid
+and itemsAndRequests.StudentID = $userid;*/
 
-
+/*Select all items of all requests that are a draft linked to a specific student*/
+/*UPDATE bursaryRequests SET bRequestsDraft = TRUE where bRequestsID =1;
+INSERT INTO itemsAndRequests(ItemID,RequestID,StudentID) VALUES(2,1,29000); FOR TESTING ONLY*/ 
+SELECT brItemDesc as 'Item', 
+IFNULL(brItemPrice,0) + IFNULL(brItemPostage,0) + IFNULL(brItemAdditionalCharges,0)
+as 'Total price', bursaryRequests.bRequestsRequestDate from bursaryRequestItems 
+inner join itemsAndRequests on bursaryRequestItems.brItemID = itemsAndRequests.ItemID 
+and itemsAndRequests.StudentID = 29000
+inner join bursaryRequests on bursaryRequests.bRequestsID = itemsAndRequests.RequestID
+and bursaryRequests.bRequestsDraft is TRUE
+ORDER BY bursaryRequests.bRequestsRequestDate DESC;
+/*FOR PHP*/
+/*SELECT brItemDesc as 'Item', 
+IFNULL(brItemPrice,0) + IFNULL(brItemPostage,0) + IFNULL(brItemAdditionalCharges,0)
+as 'Total price', bursaryRequests.bRequestsRequestDate from bursaryRequestItems 
+inner join itemsAndRequests on bursaryRequestItems.brItemID = itemsAndRequests.ItemID 
+and itemsAndRequests.StudentID = $userid
+inner join bursaryRequests on bursaryRequests.bRequestsID = itemsAndRequests.RequestID
+and bursaryRequests.bRequestsDraft is TRUE
+ORDER BY bursaryRequests.bRequestsRequestDate DESC;*/
